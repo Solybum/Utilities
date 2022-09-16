80a4c1a61f3633b4074601a4a8eb13468f27d3ab
Soleil Rojas <soleilrojas@gmail.com>
2022-09-16 02:15:16 -0600

    Add WPF utilities

---
+++ src/Soly.Utilities.WPF/FileDialogUtil.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+
+namespace Soly.Utilities.WPF;
+public static class FileDialogUtil
+{
+    public static string LoadFile(
+        string filter = "All files (*.*)|*.*",
+        string initialDirectory = "")
+    {
+        OpenFileDialog dialog = new()
+        {
+            Filter = filter,
+            InitialDirectory = initialDirectory,
+        };
+        bool? result = dialog.ShowDialog();
+        if (result == true)
+        {
+            return dialog.FileName;
+        }
+        return string.Empty;
+    }
+    public static string SaveFile(
+        string filter = "All files (*.*)|*.*",
+        string initialDirectory = "",
+        string fileName = "",
+        bool addExtension = true,
+        bool overwritePrompt = true)
+    {
+        SaveFileDialog dialog = new()
+        {
+            Filter = filter,
+            InitialDirectory = initialDirectory,
+            FileName = fileName,
+            OverwritePrompt = overwritePrompt,
+            AddExtension = addExtension,
+        };
+
+        bool? result = dialog.ShowDialog();
+        if (result == true)
+        {
+            return dialog.FileName;
+        }
+
+        return string.Empty;
+    }
+}
