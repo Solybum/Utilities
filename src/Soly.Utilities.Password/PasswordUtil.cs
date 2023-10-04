4c18003f29ca6f8650c03a8edc7da9c0eabbfaf6
Soleil Rojas <soleilrojas@gmail.com>
2023-10-03 20:41:15 -0600

    Add separate password util

---
+++ src/Soly.Utilities.Password/PasswordUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace Soly.Utilities.Password;
+public static class PasswordUtil
+{
+    public static string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+    public static bool Check(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
