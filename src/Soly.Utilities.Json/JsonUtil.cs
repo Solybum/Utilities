836390b64ceed45cd8d7e627cd91dc571bbb2cb7
Soleil Rojas <soleilrojas@gmail.com>
2023-10-03 20:41:05 -0600

    Add separate json util

---
+++ src/Soly.Utilities.Json/JsonUtil.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Soly.Utilities.Json;
+public static class JsonUtil
+{
+    public static string Serialize(object o, bool format = false, int maxDepth = 0)
+    {
+        JsonSerializerSettings settings = new()
+        {
+            Formatting = format ? Formatting.Indented : Formatting.None,
+        };
+        return JsonConvert.SerializeObject(o, settings);
+    }
+    public static T? Deserialize<T>(string json)
+    {
+        JsonSerializerSettings settings = new()
+        {
+            ObjectCreationHandling = ObjectCreationHandling.Replace,
+        };
+        return JsonConvert.DeserializeObject<T>(json, settings);
+    }
+}
