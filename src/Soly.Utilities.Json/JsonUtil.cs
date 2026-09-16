646c476cafd0dc4ed29a4bd4ac82c4cf9a863829
Soleil Rojas <soleilrojas@gmail.com>
2026-09-16 17:29:44 -0600

    Refactor JsonUtil methods to use reflection and update version to 2.2.0

--- src/Soly.Utilities.Json/JsonUtil.cs
+++ src/Soly.Utilities.Json/JsonUtil.cs
@@ -1,17 +1,33 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization.Metadata;
 
 namespace Soly.Utilities.Json;
 
 public static class JsonUtil
 {
-    public static string Serialize<T>(T data, JsonTypeInfo<T> typeInfo)
+    public static string Serialize(object o, bool format = false, int maxDepth = 0)
     {
-        return JsonSerializer.Serialize(data, typeInfo);
+        JsonSerializerOptions options = new()
+        {
+            WriteIndented = format,
+            IndentSize = 4,
+        };
+
+        if (maxDepth > 0)
+        {
+            options.MaxDepth = maxDepth;
+        }
+
+        return JsonSerializer.Serialize(o, options);
     }
 
-    public static T? Deserialize<T>(string json, JsonTypeInfo<T> typeInfo)
+    public static T? Deserialize<T>(string json)
     {
-        return JsonSerializer.Deserialize(json, typeInfo);
+        JsonSerializerOptions options = new()
+        {
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            AllowTrailingCommas = true,
+        };
+
+        return JsonSerializer.Deserialize<T>(json, options);
     }
 }
