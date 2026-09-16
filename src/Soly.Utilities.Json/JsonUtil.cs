acadaab90263e63e2dcb90121059b55a0cbdbdc8
Soleil Rojas <soleilrojas@gmail.com>
2026-09-16 17:06:30 -0600

    Refactor JsonUtil serialization methods and update version to 2.1.2

--- src/Soly.Utilities.Json/JsonUtil.cs
+++ src/Soly.Utilities.Json/JsonUtil.cs
@@ -1,48 +1,17 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Text.Json.Serialization.Metadata;
 
 namespace Soly.Utilities.Json;
 
 public static class JsonUtil
 {
-    public static string Serialize<T>(T value, JsonSerializerContext context, bool format = false) where T : class
+    public static string Serialize<T>(T data, JsonTypeInfo<T> typeInfo)
     {
-        if (context.GetTypeInfo(typeof(T)) is not JsonTypeInfo<T> typeInfo)
-        {
-            throw new InvalidOperationException(
-                $"Type {typeof(T).Name} is not registered in the provided JsonSerializerContext."
-            );
-        }
-
-        if (format)
-        {
-            var indentedOptions = new JsonSerializerOptions(typeInfo.Options)
-            {
-                WriteIndented = true,
-                IndentSize = 4
-            };
-
-            JsonTypeInfo indentedTypeInfo = indentedOptions.GetTypeInfo(typeof(T));
-
-            return JsonSerializer.Serialize(value, indentedTypeInfo);
-        }
-
-        return JsonSerializer.Serialize(value, typeInfo);
+        return JsonSerializer.Serialize(data, typeInfo);
     }
 
-    public static T? Deserialize<T>(string json, JsonSerializerContext context) where T : class
+    public static T? Deserialize<T>(string json, JsonTypeInfo<T> typeInfo)
     {
-        if (context.GetTypeInfo(typeof(T)) is not JsonTypeInfo<T> typeInfo)
-        {
-            throw new InvalidOperationException(
-                $"Type {typeof(T).Name} is not registered in the provided JsonSerializerContext."
-            );
-        }
-
-        context.Options.ReadCommentHandling = JsonCommentHandling.Skip;
-        context.Options.AllowTrailingCommas = true;
-
         return JsonSerializer.Deserialize(json, typeInfo);
     }
 }
