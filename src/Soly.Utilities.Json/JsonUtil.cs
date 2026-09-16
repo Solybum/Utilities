6839ca50033c39cbe54c0a65765cf226e8522e71
Soleil Rojas <soleilrojas@gmail.com>
2026-09-16 16:18:09 -0600

    Use System.Text.Json to avoid reflection and allow trimming. Update version and remove Newtonsoft.Json dependency

--- src/Soly.Utilities.Json/JsonUtil.cs
+++ src/Soly.Utilities.Json/JsonUtil.cs
@@ -1,22 +1,48 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 
 namespace Soly.Utilities.Json;
+
 public static class JsonUtil
 {
-    public static string Serialize(object o, bool format = false, int maxDepth = 0)
+    public static string Serialize<T>(T value, JsonSerializerContext context, bool format = false) where T : class
     {
-        JsonSerializerSettings settings = new()
+        if (context.GetTypeInfo(typeof(T)) is not JsonTypeInfo<T> typeInfo)
         {
-            Formatting = format ? Formatting.Indented : Formatting.None,
-        };
-        return JsonConvert.SerializeObject(o, settings);
+            throw new InvalidOperationException(
+                $"Type {typeof(T).Name} is not registered in the provided JsonSerializerContext."
+            );
+        }
+
+        if (format)
+        {
+            var indentedOptions = new JsonSerializerOptions(typeInfo.Options)
+            {
+                WriteIndented = true,
+                IndentSize = 4
+            };
+
+            JsonTypeInfo indentedTypeInfo = indentedOptions.GetTypeInfo(typeof(T));
+
+            return JsonSerializer.Serialize(value, indentedTypeInfo);
+        }
+
+        return JsonSerializer.Serialize(value, typeInfo);
     }
-    public static T? Deserialize<T>(string json)
+
+    public static T? Deserialize<T>(string json, JsonSerializerContext context) where T : class
     {
-        JsonSerializerSettings settings = new()
+        if (context.GetTypeInfo(typeof(T)) is not JsonTypeInfo<T> typeInfo)
         {
-            ObjectCreationHandling = ObjectCreationHandling.Replace,
-        };
-        return JsonConvert.DeserializeObject<T>(json, settings);
+            throw new InvalidOperationException(
+                $"Type {typeof(T).Name} is not registered in the provided JsonSerializerContext."
+            );
+        }
+
+        context.Options.ReadCommentHandling = JsonCommentHandling.Skip;
+        context.Options.AllowTrailingCommas = true;
+
+        return JsonSerializer.Deserialize(json, typeInfo);
     }
 }
